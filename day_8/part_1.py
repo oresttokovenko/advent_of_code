f23a68f0e6623fb73b3ed833484c71f93fa93540
Orest Tokovenko <hi@oresttokovenko.com>
2023-12-09 21:27:49 -0700

    formatting

--- day_8/part_1.py
+++ day_8/part_1.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 with open("input.txt", "r") as f:
     input = [i.strip("\n") for i in f.readlines()]
     instructions = input[0]
-    elements = [re.findall(r'\b[A-Z]{3}\b',i) for i in input[2:]]
+    elements = [re.findall(r"\b[A-Z]{3}\b", i) for i in input[2:]]
+
 
 @dataclass
 class Node:
@@ -12,27 +13,27 @@ class Node:
     left: str
     right: str
 
-nodes = [
-    Node(element=i[0], left=i[1], right=i[2])
-    for i in elements
-]
+
+nodes = [Node(element=i[0], left=i[1], right=i[2]) for i in elements]
 
 start_node = [i for i in nodes if i.element.endswith("A")][0]
 
 current_node = start_node
 steps = 0
 
+
 def find_node_by_element(nodes, target_element):
     for node in nodes:
         if node.element == target_element:
             return node
 
+
 while not current_node.element.endswith("Z"):
     for i in instructions:
         steps += 1
-        if i == 'R':
+        if i == "R":
             current_node = find_node_by_element(nodes, current_node.right)
-        elif i == 'L':
+        elif i == "L":
             current_node = find_node_by_element(nodes, current_node.left)
 
 print(steps)
