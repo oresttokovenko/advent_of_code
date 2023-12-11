502e520d54e76fb34589bb8a1ea8ba8b87d13867
Orest Tokovenko <hi@oresttokovenko.com>
2023-12-10 20:14:05 -0700

    updating day 9 solution

--- day_9/part_1.py
+++ day_9/part_1.py
@@ -1,1 +1,21 @@
-# wip
+with open("input.txt", "r") as f:
+    input = [[int(j) for j in i.strip().split()] for i in f]
+
+
+# recursively extrapolating the next value
+def extrapolate(sequence):
+    nums = []
+    for i in range(len(sequence) - 1):
+        nums.append(sequence[i + 1] - sequence[i])
+    if all(j == 0 for j in nums):
+        return sequence[-1]
+    else:
+        return sequence[-1] + extrapolate(nums)
+
+
+all_values = 0
+
+for line in input:
+    all_values += extrapolate(line)
+
+print(all_values)
