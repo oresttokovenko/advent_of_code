fdde159975eb5414ab1ffa7018e8186294ff46b7
Orest Tokovenko <hi@oresttokovenko.com>
2024-04-06 13:44:57 -0600

    solving 2015 day 2 part 2

--- 2015/day_02/part_2.py
+++ 2015/day_02/part_2.py
@@ -1,1 +1,29 @@
 # part 2
+
+import heapq
+from pathlib import Path
+
+p = Path("input.txt").read_text().rstrip("\n").split("\n")
+p = [tuple(map(lambda x: int(x), i.split("x"))) for i in p]
+
+type PresentType = list[tuple[int, ...]]
+
+
+def calc_ribbon_len(l: int, w: int, h: int) -> tuple[int, int]:
+    two_shortest = heapq.nsmallest(2, (l, w, h))
+    ribbon = two_shortest[0] + two_shortest[0] + two_shortest[1] + two_shortest[1]
+    bow = l * w * h
+    return ribbon, bow
+
+
+def wrapping_ribbon(presents: PresentType) -> int:
+    feet_of_ribbon = 0
+    for l, w, h in presents:
+        ribbon, bow = calc_ribbon_len(l, w, h)
+        feet_of_ribbon += ribbon + bow
+    return feet_of_ribbon
+
+
+if __name__ == "__main__":
+    result = wrapping_ribbon(p)
+    print(result)
