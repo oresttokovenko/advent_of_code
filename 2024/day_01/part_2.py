6725861b1b2729c75f4916fdd8a671c52d5efb54
Orest Tokovenko <hi@oresttokovenko.com>
2024-12-01 11:15:13 -0700

    removing redundant sort()

--- 2024/day_01/part_2.py
+++ 2024/day_01/part_2.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 p = Path("input.txt").read_text().split()
 
-# splitting into two lists and sorting in ascending order
-l = sorted([int(i) for i in islice(p, 0, None, 2)])
-r = sorted([int(i) for i in islice(p, 1, None, 2)])
+# splitting into two lists
+l = [int(i) for i in islice(p, 0, None, 2)]
+r = [int(i) for i in islice(p, 1, None, 2)]
 
 # counting occurrences of elements in r
 # multiplying each element in l by its count in r,
