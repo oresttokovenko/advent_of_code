8e0fb7c71af10ba5531266b2c4cb9b3ce3a51da3
Orest Tokovenko <hi@oresttokovenko.com>
2025-11-11 08:57:29 -0700

    edit

--- 2025/day_01/part_01.ml
+++ 2025/day_01/part_01.ml
@@ -1,2 +1,16 @@
-let main () = Printf.printf "Hello, Day 1 Part 1!\n"
+open CCList
+open CCIO
+open Printf
+
+let year = "2025"
+let day = "01"
+
+let lines year day file =
+  let path = sprintf "%s/day_%s/%s" year day file in
+  with_in path read_lines_l
+
+let main () =
+  let input_lines = lines year day "input.txt" in
+  iter (fun line -> printf "%s\n" line) input_lines
+
 let () = main ()
